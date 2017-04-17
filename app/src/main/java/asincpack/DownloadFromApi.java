c42c0e04416409bd633eadc524fec734e2c5317a
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-04-17 22:33:52 +0300

    update 10

---
+++ app/src/main/java/asincpack/DownloadFromApi.java
@@ -0,0 +1,95 @@
+package asincpack;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import static json.QueryUtils.LOG_TAG;
+
+/**
+ * Created by Alexandru on 4/17/2017.
+ */
+
+public class DownloadFromApi extends AsyncTask<URL, Void, String> {
+
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    @Override
+    protected String doInBackground(URL... params) {
+        String jsonResponse = "";
+
+        if (params == null) {
+            return jsonResponse;
+        }
+
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try {
+            urlConnection = (HttpURLConnection) params[0].openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.connect();
+
+            //if it was successful response code 200
+            // the read the input steam
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "STATUS CODE: " + urlConnection.getResponseCode());
+            }
+
+
+        } catch (IOException e) {
+
+            Log.e(LOG_TAG, "Problem getting the earthquake JSON results", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // function must handle java.io.IOException here
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "STATUS CODE: " + e);
+                }
+            }
+        }
+
+
+        return jsonResponse;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        Log.v(LOG_TAG, s);
+    }
+}
--- app/src/main/java/json/QueryUtils.java
+++ app/src/main/java/json/QueryUtils.java
@@ -9,18 +9,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import asincpack.DownloadFromApi;
 import model.Earthquake;
 
 /**
@@ -159,59 +156,19 @@ public class QueryUtils {
         }
 
 
-        HttpURLConnection urlConnection = null;
-        InputStream inputStream = null;
-
         try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setReadTimeout(10000 /* milliseconds */);
-            urlConnection.setConnectTimeout(15000 /* milliseconds */);
-            urlConnection.connect();
-
-            //if it was successful response code 200
-            // the read the input steam
-            if (urlConnection.getResponseCode() == 200) {
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
-            } else {
-                Log.e(LOG_TAG, "STATUS CODE: " + urlConnection.getResponseCode());
-            }
-
-
-        } catch (IOException e) {
-
-            Log.e(LOG_TAG, "Problem getting the earthquake JSON results", e);
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (inputStream != null) {
-                // function must handle java.io.IOException here
-                inputStream.close();
-            }
+            jsonResponse = new DownloadFromApi().execute(url).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            Log.e(LOG_TAG, "Async exception", e);
         }
+
+
+
         return jsonResponse;
 
     }
 
-
-    /**
-     * Convert the {@link InputStream} into a String which contains the
-     * whole JSON response from the server.
-     */
-    private static String readFromStream(InputStream inputStream) throws IOException {
-        StringBuilder output = new StringBuilder();
-        if (inputStream != null) {
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-            BufferedReader reader = new BufferedReader(inputStreamReader);
-            String line = reader.readLine();
-            while (line != null) {
-                output.append(line);
-                line = reader.readLine();
-            }
-        }
-        return output.toString();
-    }
 
 }
