8bbfd02910665d93b5576c01c1e931f0a3020a4a
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-04-20 23:54:16 +0300

    update 16

--- app/src/main/java/com/example/alexandru/earth_q_app/MainActivity.java
+++ app/src/main/java/com/example/alexandru/earth_q_app/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.alexandru.earth_q_app;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -21,12 +24,14 @@ import model.Earthquake;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Earthquake>> {
 
+    private static final int EARTHQUAKE_LOADER_ID = 1;
     private final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=5&limit=10";
     private ArrayAdapter<Earthquake> earthAdapter;
     /**
      * TextView that is displayed when the list is empty
      */
     private TextView mEmptyStateTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +56,31 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(earthAdapter);
 
-        getLoaderManager().initLoader(0, null, this);
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+
+            LoaderManager loaderManager = getLoaderManager();
+
+
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+
+        } else {
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
